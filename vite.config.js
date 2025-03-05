21581c1e473d3efd75b415056f5c01dff8ae9db5
hideANDseeksA <delmundorafer@ gmail.com>
2025-03-05 09:21:50 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 3002,
-    allowedHosts: ["*", "5bf3-122-2-31-18.ngrok-free.app"],
-  },
+  base: "/",  // ✅ Ensures correct path on Vercel
+  build: {
+    outDir: "dist", // ✅ Vercel serves from "dist"
+  }
 })
