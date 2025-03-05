5ec24a72be33fa3acff012175deed707efebf8c2
hideANDseeksA <167791229+hideANDseeksA@users.noreply.github.com>
2025-03-05 10:04:31 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,17 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3002
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Creates a separate vendor.js file
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600 // Optional: Increases the warning limit
+  },
   base: "/"
 });
