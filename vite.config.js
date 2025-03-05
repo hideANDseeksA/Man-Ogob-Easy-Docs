6c0f3e1e6cd63eb57be7ed0d8b34ede8bf5f9308
hideANDseeksA <167791229+hideANDseeksA@users.noreply.github.com>
2025-03-05 09:32:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",  // ✅ Ensures correct path on Vercel
-  build: {
-    outDir: "dist", // ✅ Vercel serves from "dist"
-  }
-})
+  server: {
+    host: "0.0.0.0",
+    port: 3002
+  },
+  base: "/"
+});
